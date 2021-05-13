3c934d2d6ba550e9cbbf963b32e8aa93eee43261
Gleb Ludis <glebludis@yandex.ru>
2021-05-13 13:09:13 +0300

    Setup babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,27 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "hello.js",
+    environment: {
+      arrowFunction: false,
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "public/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", { targets: "ie 11" }],
+          },
+        },
+      },
+    ],
+  },
 };
