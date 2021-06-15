8a76fe54bbef8d27b80ee18f32a6e7596c42c349
Gleb Ludis <glebludis@yandex.ru>
2021-06-15 14:55:23 +0300

    Refactored app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import firstRequest from "./firstRequest.js";
 import storage from "./storage.js";
 import { renderWeather } from "./userWeather.js";
 
+const form = document.querySelector("form");
+
 document.addEventListener("DOMContentLoaded", async () => {
   await firstRequest();
 });
 
-document.addEventListener("submit", async (ev) => {
+form.addEventListener("submit", async (ev) => {
   ev.preventDefault();
   await renderWeather();
 });
